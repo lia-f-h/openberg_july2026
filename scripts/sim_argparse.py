986451d82f217f42fa3b5d11d7e59a6157a2d7fe
lia-f-h <lia.f.herrmannsdorfer@ntnu.no>
2026-07-18 16:08:43 +0000

    sim iceberg2024e, start with ib2021w

--- scripts/sim_argparse.py
+++ scripts/sim_argparse.py
@@ -3,13 +3,13 @@
 #How to run. First go to directory, then run file:
     # cd ~/work/tutorials/sources/OpenDrift/openberg_july2026
     # python3 -m openberg_july2026.scripts.sim_argparse \
-    #   --argib 'iceberg2024e' \
-    #   --argoc '[["gebco","glorys"],["gebco","topaz6","topaz5"],["gebco","glophyanfcH","glophyanfcD"]]' \
+    #   --argib 'iceberg2021w' \
+    #   --argoc '[["gebco","topaz5"],["gebco","topaz6","topaz5"],["gebco","topaz4"],["gebco","glorys"]]' \
     #   --argwind '["windglophynrt"]' \
     #   --argdrift '{"wind_drag": true, "sea_ice_drag": true,  "wave_rad": false, "stokes_drift": false}' \
     #   --argname '' \
     #   --argopenberg 'lia'
-#,["gebco","topaz5"],["gebco","topaz6","topaz5"],["gebco","glophyanfcH","glophyanfcD"],["gebco","glorys"]
+
 
 # --- IMPORTS ---
 from src.utils import *
